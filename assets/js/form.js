7384b28ec286a2c75e044fb09a4581bbc59395de
rph <rhansen0127@gmail.com>
2024-08-29 23:50:06 -0500

    added function for loading the theme and updating the switcher button

--- assets/js/form.js
+++ assets/js/form.js
@@ -16,3 +16,36 @@ const lightStyle = {
   color: "black",
   backgroundColor: colorLightMode,
 };
+
+document.getElementById("day-night-tooltip").innerHTML = "Switch to dark mode!";
+loadTheme();
+
+function loadTheme() {
+  if (theme === "day") {
+    day_night.src = sunPic;
+
+    for (const element of background) {
+      for (const key in lightStyle) {
+        element.style[key] = lightStyle[key];
+      }
+    }
+    for (const element of labels) {
+      element.style.color = "black";
+    }
+    document.getElementById("day-night-tooltip").innerHTML =
+      "Switch to dark mode!";
+  } else {
+    day_night.src = moonPic;
+
+    for (const element of background) {
+      for (const key in darkStyle) {
+        element.style[key] = darkStyle[key];
+      }
+    }
+    for (const element of labels) {
+      element.style.color = "white";
+    }
+    document.getElementById("day-night-tooltip").innerHTML =
+      "Switch to light mode!";
+  }
+}
